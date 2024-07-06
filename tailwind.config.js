35434e8ae11911d76b0c51d2c7b7c26814eeadf0
sanchxt <tpbeast007@gmail.com>
2024-07-06 23:11:47 +0530

    add animation keyframes

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,51 @@ export default {
             transform: "scale(1)",
           },
         },
+        traversed: {
+          "0%": {
+            transform: "scale(0.3)",
+            backgroundColor: "#9333eafb",
+            borderRadius: "100%",
+          },
+          "50%": {
+            backgroundColor: "#4f46e5bf",
+          },
+          "75%": {
+            transform: "scale(1.2)",
+            backgroundColor: "#3b82f6bf",
+          },
+          "100%": {
+            transform: "scale(1)",
+            backgroundColor: "#22d3ee",
+          },
+        },
+        path: {
+          "0%": {
+            transform: "scale(0.3)",
+            backgroundColor: "#e11d48df",
+            borderRadius: "100%",
+          },
+          "50%": {
+            backgroundColor: "#ea580cbf",
+          },
+          "75%": {
+            transform: "scale(1.2)",
+            backgroundColor: "#fb923cbf",
+          },
+          "90%": {
+            transform: "scale(0.8)",
+            backgroundColor: "#fde68a",
+          },
+          "100%": {
+            transform: "scale(1)",
+            backgroundColor: "#22d3ee",
+          },
+        },
+      },
+      animation: {
+        traversed: "traversed 0.5s cubic-bezier(0, 0, 0.2, 1)",
+        path: "path 1.5s cubic-bezier(0, 0, 0.2, 1)",
+        wall: "wall 0.3s cubic-bezier(0.4, 0, 0.2, 1)",
       },
     },
   },
